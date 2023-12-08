957324e4748635682399f33e98e944e4af0dea34
Abylai <124864191+abylai123-tech@users.noreply.github.com>
2023-12-08 18:55:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,36 @@
-const http = require('http');
-const fs = require('fs').promises;
+const http=require('http')
 
-const filename = 'students.json';
-let students = [];
+let student=[]
 
-async function readStudentsFromFile() {
-  try {
-    const data = await fs.readFile(filename, 'utf-8');
-    students = JSON.parse(data);
-  } catch (error) {
-    students = [];
-  }
-}
+http.createServer((request,response)=>{
 
-async function writeStudentsToFile() {
-    await fs.writeFile(filename, JSON.stringify(students, null, 2), 'utf-8');
-  }
+  console.log(request.url)
   
-const server = http.createServer(async (request, response) => {
-  if (request.method === 'POST') {
-    let data = '';
-
-    request.on('data', chunk => {
-      data += chunk;
-    });
-
-    request.on('end', async () => {
-      const studentName = data.trim();
-      students.push(studentName);
-      await writeStudentsToFile();
-      console.log(students);
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end('<h1>OK</h1>');
-    });
-  } else if (request.method === 'GET') {
-    const index = parseInt(request.url.slice(1));
-
-    await readStudentsFromFile();
-
-    if (students.length > 0 && index >= 0 && index < students.length) {
-      const studentName = students[index];
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end(studentName);
-    } else {
-      response.writeHead(404, { 'Content-Type': 'text/html' });
-      response.end('<h1>Not found</h1>');
-    }
-  } else if (request.method === 'DELETE') {
-    const studentNameToDelete = request.url.slice(1);
-
-    if (students.includes(studentNameToDelete)) {
-      students = students.filter(student => student !== studentNameToDelete);
-      await writeStudentsToFile();
-      console.log(students);
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end('<h1>OK</h1>');
-    } else {
-      response.writeHead(404, { 'Content-Type': 'text/html' });
-      response.end('<h1>Not found</h1>');
-    }
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end('<h1>Not found</h1>');
+  if(request.url=='/student'){
+    response.statusCode=200;
+    response.end('<h1>hello student</h1>');
   }
-});
+  else if(request.url=='/student/num'){
+    response.statusCode=200;
+    response.end('<h1>hello student num</h1>');
+  }
+  else if(request.url=="/teacher"){
+    response.statusCode=200;
+    response.end('<h1>hello teacher</h1>');
+  }
+  else{
+    response.statusCode=404
+    response.end('<h1>NotFound</h1>');
+  }
 
-const port = 3000;
+//  if(request.method=='POST'){
+//   student.push(request.url.slice(1))
+//   console.log(student)
+//   response.end('<h1>студент добавлен</h1>')
+//  }
 
-server.listen(port, async () => {
-  await readStudentsFromFile();
-  console.log(`Server is running on port ${port}`);
-});
+//  else if(request.method=='GET'){
+//   console.log(student)          
+//   response.end(`<h1>${student[request.url.slice(1)]}</h1>`)
+//  }
+}).listen(3000);
